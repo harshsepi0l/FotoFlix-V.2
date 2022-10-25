d82b93e39e3e7f1ac239c090e31318964658cf6e
Fatima Ortega <fatimarortega@gmail.com>
2022-10-25 15:56:46 -0400

    Added files to main folder

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,24 @@
+import { Col, Row } from "antd";
+import { CustomHeader } from "../../components/Common/CustomHeader";
+import { TrendingImages } from "../../components/Common/TrendingImages";
+import { TrendingTags } from "../../components/Common/TrendingTags";
+import { AccountBar } from "../../components/HomePage/AccountBar";
+import { AccountInfo } from "../../components/HomePage/AccountInfo";
+
+export function SignUp(): JSX.Element {
+  return(
+    <div>
+    <CustomHeader isLoggedIn={true} />
+    <Row>
+      <Col span={18}>
+        <AccountInfo/>
+        <AccountBar/>
+      </Col>
+      <Col span={6}>
+        <TrendingImages />
+        <TrendingTags />
+      </Col>
+    </Row>
+  </div>
+  )
+}
