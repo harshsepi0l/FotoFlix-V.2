369757900f3536b886cea9c4770348ca21ccee1a
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-10-26 17:35:06 -0400

    Update index.js

--- flixserver/index.js
+++ flixserver/index.js
@@ -11,8 +11,7 @@ const db = mysql.createPool({
   password: 'Hxrsh295'
 })
 
-app.use(express.json());
-app.use(cors());
+
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -37,6 +36,8 @@ app.post('/api/insert', (req,res) => {
   )
 })
 
+app.use(express.json());
+app.use(cors());
 
 app.listen(3000, () => {
   console.log('running on port 3000')
