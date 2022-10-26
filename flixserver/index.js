86ad5348686152911787a6371bb053c48acd28c4
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-10-26 11:52:58 -0400

    changes

--- flixserver/index.js
+++ flixserver/index.js
@@ -7,13 +7,33 @@ const mysql = require('mysql2')
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: 'MsgPstrDB',
-  password: 'password'
+  database: 'flixers',
+  password: 'Hxrsh295'
+})
+app.get('/api/get',(req,res) => {
+  const sqlSelect = 'SELECT * FROM flixers'
+
+  db.query(sqlSelect, (err,result) => {
+    res.send(result)
+  })
 })
 
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.post('/api/insert', (req,res) => {
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+
+  const sqlInsert = "INSERT INTO flixers (username, email, password) VALUES (?,?,?)"
+  db.query(sqlInsert, [username, email, password], (err,result) => {
+    console.log(result)
+  })
+})
+
 
 app.listen(3000, () => {
   console.log('running on port 3000')
