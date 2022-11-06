01b88e13de5b74fd067f197ce6e228c454cddbd5
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-06 12:58:58 -0500

    changed

--- tstest/src/pages/Login/index.tsx
+++ tstest/src/pages/Login/index.tsx
@@ -11,25 +11,48 @@ import { AccountInfo } from "../../components/HomePage/AccountInfo";
 export const Login = () => {
   const [username, checkUsername] = useState("");
   const [password, checkPassword] = useState("");
-  const [loginStatus, setLoginStatus] = useState("");
+  const [loginStatus, setLoginStatus] = useState(false);
+
   const navigate = useNavigate();
 
   // This will navigate to Login Page once user has logged in
   const sendToHomePage = () => {
     navigate("/HomePage");
   };
+  Axios.defaults.withCredentials = true;
 
   const loginCheck = () => {
     Axios.post("http://localhost:3000/api/login", {
       Username: username,
       Password: password,
     }).then((response) => {
-      if (response.data.message) {
-        setLoginStatus(response.data.message);
+      console.log(response);
+      if (!response.data.auth) {
+        setLoginStatus(false);
       } else {
-        setLoginStatus(response.data[0].Username);
-        sendToHomePage();
+        console.log(response.data);
+        localStorage.setItem("token", response.data.token);
+        setLoginStatus(true);
+        // sendToHomePage();
       }
+    });
+  };
+  /*
+  useEffect(() => {
+    Axios.get("http://localhost:3000/api/Login").then((response) => {
+      if (response.data.loggedIn == true) {
+        setLoginStatus(response.data.user[0].Username);
+      }
+    });
+  }, []);
+  
+  */
+  // Deleteable functions
+  const userAuthenticated = () => {
+    Axios.get("http://localhost:3000/isUserAuth", {
+      headers: { "x-access-token": localStorage.getItem("token") },
+    }).then((response) => {
+      console.log(response);
     });
   };
 
@@ -66,8 +89,10 @@ export const Login = () => {
       </div>
       <div className="login">
         <button type="button" className="btn" onClick={loginCheck} />
-        <h1>{loginStatus}</h1>
       </div>
+      {loginStatus && (
+        <button onClick={userAuthenticated}> Check if Authenticated </button>
+      )}
     </div>
   );
 };
