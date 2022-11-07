618cdebb9eb0cb11a6c35cabd23593bce6a4e78f
Fatima Ortega <fatimarortega@gmail.com>
2022-11-07 14:49:09 -0500

    Update App.tsx

--- tstest/src/App.tsx
+++ tstest/src/App.tsx
@@ -3,19 +3,18 @@ import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
-import { ChipsArray } from "./components/Tags";
-import UploadImage  from "./components/ImageUploadPage";
+
 
 function App() {
   return (
-    // // <Router>
-    // //   <Routes>
-    // //     <Route path="/HomePage" element={<HomePage />} />
-    // //     <Route path="/Login" element={<Login />} />
-    // //     <Route path="/" element={<SignUp />} />
-    // //   </Routes>
-    // // </Router>
-    <UploadImage />
+    <Router>
+      <Routes>
+        <Route path="/HomePage" element={<HomePage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<SignUp />} />
+      </Routes>
+    </Router>
+   
   );
 }
 
