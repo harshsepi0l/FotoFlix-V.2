8b55f9dca08bb833bb03ed486c0a49250e9248f8
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-07 02:05:12 -0500

    changed

--- tstest/src/App.tsx
+++ tstest/src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { HomePage } from "./pages/HomePage";
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
+import { LandingPage } from "./pages/LandingPage";
 import { ChipsArray } from "./components/Tags";
 
 function App() {
