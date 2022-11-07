026ef5f6b0651f5e2e7b58a697b2cbda88735a6d
schrmad <104103941+schrmad@users.noreply.github.com>
2022-11-07 15:07:50 -0500

    commit

--- tstest/src/components/ImageUpload/ImageForm.tsx
+++ tstest/src/components/ImageUpload/ImageForm.tsx
@@ -1,10 +1,13 @@
-import React, { createElement, useState } from 'react';
+import React, { createElement, useState, useEffect } from 'react';
 /** A form for uploading an image (a foto!!)
  * @author Madeline Schroeder
  * @returns 
  * @version 11.6.22
  * 
- *  Tutorials:
+ *  Steps:
+ *  User is logged in
+ *  User clicks choose file button
+ *  User Enters information about the image such as title description etc.
  *  
  */
 
@@ -14,10 +17,37 @@ export function ImageForm(): JSX.Element {
     return (
         <div>
             <h1>Upload your image to fotoflix!</h1>
-            <form>
-                <input type="file" name="foto"></input>
-                <button>Upload</button>
-            </form>
+            <div className="form">
+                <label>Upload an image: </label>
+                <input
+                type="file"
+                name="image"
+                onChange={(e) => {
+                    //
+                }}
+                />
+
+                <label>Title your image:</label>
+                <input
+                type="text"
+                name="title"
+                onChange={(e) => {
+                    //
+                }}
+                />
+
+                <label>Describe your image:</label>
+                <input
+                type="text"
+                name="description"
+                />
+
+                <label>Is this image public or private?</label>
+                <select>
+                    <option value="public">Public</option>
+                    <option value="private">Private</option>
+                </select>
+            </div>
         </div>
     )
 }
--- tstest/src/pages/ImageUpload/index.tsx
+++ tstest/src/pages/ImageUpload/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react"; // Importing things for React
 import "./ImageUpload.css"; // Import the css for this page
 import Axios from "axios"; // Importing Axios
-import { ImageForm } from "../../components/ImageUpload/ImageForm";
+import { ImageForm } from "../../components/ImageUpload/ImageForm"; // This shouldn't be an error but it is!!
 
 // Image uploading page
 /** A page for a logged-in user to upload an image to the website.
