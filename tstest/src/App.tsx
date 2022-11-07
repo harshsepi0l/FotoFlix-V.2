c9d8c345a66b4905b305ad895a406a18991757d1
Fatima Ortega <fatimarortega@gmail.com>
2022-11-07 17:40:23 -0500

    Update App.tsx

--- tstest/src/App.tsx
+++ tstest/src/App.tsx
@@ -4,6 +4,7 @@ import { HomePage } from "./pages/HomePage";
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
 import { ChipsArray } from "./components/Tags";
+import { LandingPage } from "./pages/LandingPage";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Routes>
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="/" element={<SignUp />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </Router>
     // <ChipsArray />
