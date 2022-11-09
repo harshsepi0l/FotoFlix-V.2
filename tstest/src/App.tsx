de97779aba32fb3c429799937168f4505bc38732
Fatima Ortega <fatimarortega@gmail.com>
2022-11-08 23:29:35 -0500

    Update App.tsx

--- tstest/src/App.tsx
+++ tstest/src/App.tsx
@@ -5,16 +5,19 @@ import { ImagesForm } from "./pages/ImagesFolder";
 import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
 import { LandingPage } from "./pages/LandingPage";
+import { ImagePage } from "./pages/ImagePage";
 
 function App() {
   return (
+   
+
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="/ImagesFolder" element={<ImagesForm />} />
         <Route path="/HomePage" element={<HomePage />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/ImagesFolder" element={<ImagesForm />} />
+        <Route path="/" element={<ImagePage />} />
       </Routes>
     </Router>
   );
