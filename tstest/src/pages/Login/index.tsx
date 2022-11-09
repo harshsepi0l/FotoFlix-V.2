717bd166392edcf599e4462820521ffd6fffb503
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-09 00:18:37 -0500

    Update index.tsx

--- tstest/src/pages/Login/index.tsx
+++ tstest/src/pages/Login/index.tsx
@@ -111,18 +111,6 @@ export const Login = () => {
               onClick={loginCheck}
             />
           </div>
-          <br />
-          <NavLink onClick={sendToSignUp} to={"/SignUp"}>
-            <p style={{ color: "#C689C6" }}>Don't have an account?</p>
-          </NavLink>
-        </div>
-        <div className="login">
-          <CustomButton
-            buttonType={"primary"}
-            color={"darkpurple"}
-            title={"Login"}
-            onClick={loginCheck}
-          />
         </div>
       </div>
     </div>
