b5e52d50969d95d2957f816a04d14bf9abc9dcb3
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-13 14:44:07 -0500

    changes

--- tstest/src/pages/HomePage/index.tsx
+++ tstest/src/pages/HomePage/index.tsx
@@ -1,8 +1,4 @@
 import { Col, Row } from "antd";
-<<<<<<< HEAD
-import { useEffect } from "react";
-=======
->>>>>>> windows-harsha
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { CustomHeader } from "../../components/Common/CustomHeader";
@@ -10,19 +6,8 @@ import { TrendingImages } from "../../components/Common/TrendingImages";
 import { TrendingTags } from "../../components/Common/TrendingTags";
 import { AccountBar } from "../../components/HomePage/AccountBar";
 import { AccountInfo } from "../../components/HomePage/AccountInfo";
-import "./index.css";
 
 export function HomePage(): JSX.Element {
-<<<<<<< HEAD
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      navigate("/LandingPage");
-    }
-  }, []);
-
-=======
->>>>>>> windows-harsha
   return (
     <div>
       <CustomHeader isLoggedIn={true} />
--- tstest/src/pages/SignUp/index.tsx
+++ tstest/src/pages/SignUp/index.tsx
@@ -1,113 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-import fotoLogo from "../../components/ImageLogo/fotoLogo.svg";
-import { Button, Input, Space } from "antd";
-import { CustomButton } from "../../components/Common/CustomButton";
-
-import "./signUp.css";
-import Axios from "axios";
-
-export const SignUp = () => {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [username, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [usersList, setUsersList] = useState<any>([]);
-  const [newUsername, setNewUsername] = useState("");
-  const navigate = useNavigate();
-
-  // This will navigate to Login Page once user has signed up
-  const sendToLogin = () => {
-    navigate("/Login");
-  };
-
-  // This will navigate to Login Page once user has signed up
-  const sendToLanding = () => {
-    navigate("/");
-  };
-
-  const refreshPage = () => {
-    window.location.reload();
-  };
-
-  useEffect(() => {
-    Axios.get("http://localhost:3000/api/get").then((response) => {
-      setUsersList(response.data);
-    });
-  }, []);
-
-  const submitLog = () => {
-    
-    Axios.post("http://localhost:3000/api/registration", {
-      Firstname: firstname,
-      Lastname: lastname,
-      Username: username,
-      Email: email,
-      Password: password,
-    });
-
-    setUsersList([
-      ...usersList,
-      {
-        Firstname: firstname,
-        LastName: lastname,
-        Username: username,
-        Email: email,
-        Password: password,
-      },
-    ]);
-    //  sendToLogin();
-    
-  };
-
-  const deleteAccount = (
-    Username:
-      | string
-      | number
-      | boolean
-      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-      | React.ReactFragment
-      | React.ReactPortal
-      | null
-      | undefined
-  ) => {
-    Axios.delete(`http://localhost:3000/api/delete/${Username}`);
-    refreshPage();
-  };
-
-  const updateUserName = (
-    firstname:
-      | string
-      | number
-      | boolean
-      | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-      | React.ReactFragment
-      | React.ReactPortal
-      | null
-      | undefined
-  ) => {
-    Axios.put(`http://localhost:3000/api/update/`, {
-      Firstname: firstname,
-      Username: newUsername,
-    });
-    setNewUsername("");
-  };
-
-  return (
-    <div  style={{ marginTop: 5}}>
-     
-    <div className="Container">
-        <div className="App">
-        <img
-        style={{ color: "#937DC2", width: 200, height: 90}}
-        src={fotoLogo}
-        alt="error"
-          />
-          
-=======
 import "./signUp.css";
 import Axios from "axios";
 
@@ -189,20 +82,12 @@ export const SignUp = () => {
   return (
     <div className="Container">
       <div className="App">
->>>>>>> windows-harsha
         <h1 style={{ color: "#937DC2" }}>Sign up for a free account</h1>
         <div className="SignUpContainer">
           {/* This is a row */}
           <div className="Column">
             <div className="Row">
               <div className="form">
-<<<<<<< HEAD
-                <Input
-                  placeholder="Username"
-                  type="text"
-                  name="username"
-                  style={{ borderRadius: "25px", height: "82.64px" }}
-=======
                 <label>First name: </label>
                 <input
                   type="text"
@@ -227,22 +112,13 @@ export const SignUp = () => {
                 <input
                   type="text"
                   name="username"
->>>>>>> windows-harsha
                   onChange={(e) => {
                     setUserName(e.target.value);
                   }}
                 />
-<<<<<<< HEAD
-                <br />
-                <br />
-                <Input
-                  placeholder="Email Address"
-                  style={{ borderRadius: "25px", height: "82.64px" }}
-=======
 
                 <label> Email: </label>
                 <input
->>>>>>> windows-harsha
                   type="text"
                   name="email"
                   onChange={(e) => {
@@ -250,56 +126,6 @@ export const SignUp = () => {
                   }}
                 />
               </div>
-<<<<<<< HEAD
-              <br />
-
-              {/* This is a column */}
-              <div className="Row">
-                <Input
-                  placeholder="First Name"
-                  type="text"
-                  name="firstname"
-                  style={{
-                    borderRadius: "25px",
-                    height: "82.64px",
-                    width: "20vw",
-                    marginRight: "40px",
-                  }}
-                  onChange={(e) => {
-                    setFirstname(e.target.value);
-                  }}
-                />
-
-                <Input
-                  placeholder="Last Name"
-                  type="text"
-                  name="lastname"
-                  style={{
-                    borderRadius: "25px",
-                    height: "82.64px",
-                    width: "20vw",
-                    marginLeft: "16px",
-                  }}
-                  onChange={(e) => {
-                    setLastname(e.target.value);
-                  }}
-                />
-              </div>
-              <br />
-              <div>
-                <Space direction="vertical">
-                  <Input.Password
-                    placeholder="Create Password"
-                    style={{ borderRadius: "25px", height: "82.64px" }}
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                    }}
-                  />
-                </Space>
-              </div>
-            </div>
-            <br />
-=======
               <label>Password: </label>
               <input
                 type="text"
@@ -310,38 +136,13 @@ export const SignUp = () => {
               />
             </div>
 
->>>>>>> windows-harsha
             <NavLink onClick={sendToLogin} to={"/Login"}>
               <p style={{ color: "#C689C6" }}>Already have an account?</p>
             </NavLink>
           </div>
 
-<<<<<<< HEAD
-          <div className="Button-SignUp">
-            <CustomButton
-              buttonType={"primary"}
-              color={"darkpurple"}
-              title={"Register"}
-              onClick={submitLog}
-              
-            />
-
-            <div className="margin-Register-signup">
-              <NavLink onClick={sendToLanding} to={"/"}>
-                <CustomButton
-                  buttonType={"primary"}
-                  color={"red"}
-                  title={"Cancel"}
-                />
-              </NavLink>
-            </div>
-          </div>
-          </div>
-          </div>
-=======
           <button onClick={submitLog}>Register</button>
         </div>
->>>>>>> windows-harsha
 
         {/* {usersList.map(
           (key: {
