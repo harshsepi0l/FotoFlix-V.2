d88208718716853eca13735820961e53ab216aee
tachau03 <taminhchau8903@gmail.com>
2022-11-13 23:16:03 -0500

    Update TrendingImages

--- tstest/src/components/Common/TrendingImages.tsx
+++ tstest/src/components/Common/TrendingImages.tsx
@@ -1,28 +1,31 @@
 import { Card } from "antd";
-import { useState } from "react";
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
 import { CustomCard } from "./CustomCard";
 import { InfiniteScroll } from "./InfiniteScroll";
 
+interface CardProps {
+  PublicOrPrivate: number;
+  Url: string;
+  Title: string;
+  Description: string;
+  Dislike: number;
+  isScroll?: boolean;
+  Avatar: string;
+  Like: number;
+  Tags: string;
+  Favorite: number;
+  key: any;
+}
+
 export function TrendingImages(): JSX.Element {
-  const NUMBERS_PER_PAGE = 100;
-
-  const [numbers, setNumbers] = useState<number[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(0);
-
-  const hasMoreData = numbers.length < 1000;
-
-  const loadMoreNumbers = () => {
-    setPage((page) => page + 1);
-    setLoading(true);
-    setTimeout(() => {
-      const newNumbers = new Array(NUMBERS_PER_PAGE)
-        .fill(1)
-        .map((_, i) => page * NUMBERS_PER_PAGE + i);
-      setNumbers((nums) => [...nums, ...newNumbers]);
-      setLoading(false);
-    }, 300);
-  };
+  const [values, setValues] = React.useState<CardProps[]>([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/Cloudinary").then((response) => {
+      setValues(response.data);
+    });
+  }, []);
 
   return (
     <Card title="Trending Images"
@@ -31,28 +34,22 @@ export function TrendingImages(): JSX.Element {
 
         height: 300, wordWrap: "break-word", overflowY: "scroll", scrollBehavior: "smooth"
       }}>
-        <InfiniteScroll
 
-          hasMoreData={hasMoreData}
-          isLoading={loading}
-          onBottomHit={loadMoreNumbers}
-          loadOnMount={true}
-        >
-          < ul >
-            {
-              numbers.map(() => (
-                <CustomCard
-                  key={undefined}
-                  PublicOrPrivate={0}
-                  Url={""} Title={""}
-                  Description={""} Dislike={0}
-                  Avatar={""} Like={0}
-                  Tags={""}
-                  Favorite={0} />
-              ))
-            }
-          </ul>
-        </InfiniteScroll >
+        {
+          values.map((value) => (
+            <CustomCard
+              key={value.key}
+              PublicOrPrivate={value.PublicOrPrivate}
+              Url={value.Url}
+              Title={value.Title}
+              Description={value.Description}
+              Dislike={value.Dislike}
+              Avatar={value.Avatar}
+              Like={value.Like}
+              Tags={value.Tags}
+              Favorite={value.Favorite} />
+          ))
+        }
       </div>
     </Card >
   )
