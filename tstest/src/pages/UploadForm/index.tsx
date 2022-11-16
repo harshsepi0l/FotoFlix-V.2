79517b83906be8c3e09e35486f35befe92248b50
Fatima Ortega <fatimarortega@gmail.com>
2022-11-15 19:56:22 -0500

    Update index.tsx

--- tstest/src/pages/UploadForm/index.tsx
+++ tstest/src/pages/UploadForm/index.tsx
@@ -9,9 +9,12 @@ import {
   FormikValues,
 } from "formik";
 //import { ChipsArray } from "../../components/Tag";
+import { useNavigate } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import { Chip, Paper } from "@mui/material";
 import { Box, Button, TextField } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { Alert } from "antd";
 
 export interface ChipData {
   key: number;
@@ -29,6 +32,13 @@ export const UploadForm = () => {
   const [imageDesc, setImageDesc] = useState(""); // Control image description
   const [imageVis, setImageVis] = useState(1); // Control public (0) vs. private (1)
   const [imageTags, setImageTags] = useState(""); // tags
+  const navigate = useNavigate();
+
+  // This will navigate to Landing Page once user has signed up
+  const sendToLanding = () => {
+    navigate("/");
+    // <Alert message="Success Text" type="success" />;
+  };
 
   const handleTagsChange = (e: any) => {
     const tags = e.target.value;
@@ -51,23 +61,24 @@ export const UploadForm = () => {
     setInput("");
   };
 
-  const handleImageTitleChange = (e: any) => { // This updates every single time anything is typed
+  const handleImageTitleChange = (e: any) => {
+    // This updates every single time anything is typed
     const title = e.target.value;
     setImageTitle(title);
-  }
+  };
   const handleImageDescChange = (e: any) => {
     const desc = e.target.value;
     setImageDesc(desc);
-  }
-  const handleImageVisChange = (e: any) => {// Public or private
+  };
+  const handleImageVisChange = (e: any) => {
+    // Public or private
     const vis = e.target.value;
-    if (vis == "public"){
+    if (vis == "public") {
       setImageVis(0);
-    }
-    else {
+    } else {
       setImageVis(1);
     }
-  }
+  };
   // const handleTagsChange = (e: any) => {
   //   const tags = e.target.value;
   //   setImageTags(tags);
@@ -90,17 +101,18 @@ export const UploadForm = () => {
     e.preventDefault();
     if (!previewSource) return;
     uploadImage(previewSource);
+    navigate("/");
   };
   const uploadImage = async (base64EncodedImage: any) => {
     try {
       await fetch("http://localhost:3000/Cloudinary", {
         method: "POST",
-        body: JSON.stringify({ 
+        body: JSON.stringify({
           data: base64EncodedImage,
           Title: imageTitle,
           Description: imageDesc,
           PublicOrPrivate: imageVis,
-          Tags: chipData
+          Tags: chipData,
         }),
         headers: { "Content-type": "application/json" },
       });
@@ -130,7 +142,8 @@ export const UploadForm = () => {
       <div>
         <h1>Upload </h1>
         <form onSubmit={handleSubmitFile} className="form">
-          <h3>Upload your image!</h3><br />
+          <h3>Upload your image!</h3>
+          <br />
           <input
             type="file"
             name="image "
@@ -138,27 +151,40 @@ export const UploadForm = () => {
             value={fileInputState}
             className="form-input"
           />
-          <br /><h3>Image Title</h3>
+          <br />
+          <h3>Image Title</h3>
           <input
             type="text"
             name="Image Title"
             onChange={handleImageTitleChange}
             className="form-input"
           />
-          <br /><h3>Image Description</h3>
-          <input 
+          <br />
+          <h3>Image Description</h3>
+          <input
             type="text"
             name="Image Description"
             onChange={handleImageDescChange}
             className="form-input"
           />
-          <br /><h3>Public or private?</h3>
-          <input type="radio" value="public" id="public"
-                onChange={handleImageVisChange} name="vis" />
+          <br />
+          <h3>Public or private?</h3>
+          <input
+            type="radio"
+            value="public"
+            id="public"
+            onChange={handleImageVisChange}
+            name="vis"
+          />
           <label htmlFor="public">Public</label>
           <br />
-          <input type="radio" value="private" id="private"
-            onChange={handleImageVisChange} name="vis"/>
+          <input
+            type="radio"
+            value="private"
+            id="private"
+            onChange={handleImageVisChange}
+            name="vis"
+          />
           <label htmlFor="private">Private</label>
 
           <br />
@@ -167,9 +193,9 @@ export const UploadForm = () => {
               <TextField
                 placeholder="tag name"
                 value={input}
-                onChange={(e: { target: { value: React.SetStateAction<string> } }) =>
-                  setInput(e.target.value)
-                }
+                onChange={(e: {
+                  target: { value: React.SetStateAction<string> };
+                }) => setInput(e.target.value)}
               />
               <Button onClick={handleClick}> Save tag</Button>
             </Box>
@@ -199,9 +225,14 @@ export const UploadForm = () => {
               })}
             </Paper>
           </Box>
-          <button className="btn" type="submit">
-            Submit
-          </button>
+          <div>
+            <NavLink onClick={sendToLanding} to={"/"}>
+              <button className="btn" type="submit">
+                Submit
+              </button>
+             
+            </NavLink>
+          </div>
         </form>
         {previewSource && (
           <img src={previewSource} alt="chosen" style={{ height: "300px" }} />
