3a3186692d5898e270d13bd7cb9e778d49627257
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-16 11:54:15 -0500

    changed

--- tstest/src/components/HomePage/AccountInfo.tsx
+++ tstest/src/components/HomePage/AccountInfo.tsx
@@ -2,7 +2,6 @@ import { AntDesignOutlined } from "@ant-design/icons";
 import { Avatar, Button, Col, Row, Typography } from "antd";
 import Axios from "axios";
 import React, { useEffect } from "react";
-import BaseModalWrapper from "./ModalPopup/BaseModalWrapper";
 import "./index.css";
 
 export function AccountInfo(): JSX.Element {
@@ -34,22 +33,9 @@ export function AccountInfo(): JSX.Element {
         </Col>
         <Col flex={5}>
           <Typography.Title level={2} className="HomePage-AccountInfo">
-            {user?.username}
+            {/* {user?.username} */}
             {/* {editableStr} */}
           </Typography.Title>
-
-          <Button
-            size="large"
-            onClick={toggleModal}
-            className="HomePage-EditButton"
-          >
-            Delete Account
-          </Button>
-          <BaseModalWrapper
-            isModalVisible={isModalVisible}
-            onBackdropClick={toggleModal}
-            header={"Deleted"}
-          />
         </Col>
       </Row>
     </div>
