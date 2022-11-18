8daac598a0151c9c7a0e1045a4543b5504b516f4
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-18 03:45:52 -0500

    Update index.js

--- flixserver/index.js
+++ flixserver/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(cors());
+
 require("dotenv").config();
 const bodyParser = require("body-parser");
 
-app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 app.use(
   express.urlencoded({
