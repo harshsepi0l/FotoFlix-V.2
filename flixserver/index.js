30ab79220f14c12c21b0d9ca472271d132d3a92b
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-18 03:49:37 -0500

    Update index.js

--- flixserver/index.js
+++ flixserver/index.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const app = express();
 const cors = require("cors");
 app.use(cors());
+
+const express = require("express");
+const app = express();
 
 require("dotenv").config();
 const bodyParser = require("body-parser");
