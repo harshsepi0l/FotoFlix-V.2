ec894d3aba7b10cc43b95051ee8b506970a2bae0
Harsha Ajjarapu <49374020+harshsepi0l@users.noreply.github.com>
2022-11-18 03:15:22 -0500

    changes

--- flixserver/routes/flixertaps.js
+++ flixserver/routes/flixertaps.js
@@ -3,31 +3,18 @@ const router = express.Router();
 const { sequelize } = require("../models");
 const { DataTypes } = require("sequelize");
 const post = require("../models/post");
-const flixertaps = require("../models/post")(sequelize, DataTypes)
-
+const flixertaps = require("../models/post")(sequelize, DataTypes);
 
 sequelize, DataTypes;
 const { validateToken } = require("../middlewares/AuthMiddleware");
 
-router.get("/", async (req, res) => {
-  const postid = req.params.postid;
-  const posts = await post.findAll({
-    where: {
-      postid: postid,
-    },
-  });
-  flixertaps = await flixertaps.findAll({
-    where: {
-      postid: postid,
-    },
-  });   
-  res.json(posts);
-});
+// router.get("/", async (req, res) => {
+//   await.create({
+
+// });
 
 // router.post("/like".async(req, res)) => {
 //     const postid = req.params.postid;
 //     const post
 // }
 module.exports = router;
-
-
